f61da4a0f7fb81e1e3dd955bc0df1853d660a8c7
Hariharaprasad-03 <hariharaprasad03@gmail.com>
2025-11-04 09:03:31 +0530

    4-11-2025 9.00 AM - newAddition

---
+++ Patterns/src/FacadeDesign/Facade.java
@@ -0,0 +1,8 @@
+package FacadeDesign;
+
+public class Facade {
+
+//    Structural Design Pattern
+//    that simplifies access to a complex subsystem by providing a unified, high-level interface.
+
+}
